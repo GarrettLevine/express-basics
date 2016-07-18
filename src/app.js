b76dfc605966f7d739a4a80016933fd0ca28235f
Garrett Levine <garrlevine@gmail.com>
2016-07-17 21:47:18 -0400

    starting templates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 'use strict';
 
 const express = require('express');
+const posts = require('./mock/posts.json');
+
 const app = express();
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.send('<h1>I REALLY love Express!</h1>');
+});
+
+app.get('/blog/:title?', (req, res) => {
+  const title = req.params.title;
+  if ( title === undefined ) {
+    res.status(503);
+    res.send('This page is under construction');
+  } else {
+    const post = posts[title];
+
+    res.send(post);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Node front-end server is running on port 3000!')
+});
+
